3ceaf7e5af0de20fe2d93768451c7fc544946676
siva-guru0208 <sivaguru0208@gmail.com>
2025-04-11 20:18:39 +0530

    Add files via upload

---
+++ drizzle.config.js
@@ -0,0 +1,16 @@
+
+
+export default  {
+    schema: "./utils/schema.js",
+    dialect:"mysql",
+    
+    
+    dbCredentials: {
+        host: "webtracking-webtracking.i.aivencloud.com",
+        user: "avnadmin",
+        database: "defaultdb",
+        password:"AVNS_YT2KmvvbS0vuH41rdpA",
+        port:"16632",
+        
+    }
+  };
